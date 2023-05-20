ccb62311b8c4d9c4508c809127336073bd3b57ec
alopacin <adlopacinski@gmail.com>
2023-05-20 18:07:27 +0200

    zrobiony ogolny zarys programu, nadal nie dziala jak powinno

--- main.py
+++ main.py
@@ -11,13 +11,28 @@
     Napisz program tak, by wysokość początkowego kredytu, oprocentowanie kredytu (ponad inflację) i kwota raty były pobierane ze standardowego wejścia (terminal).
     Przykładowe wartości kredytu i formułę do jego wyliczenia znajdziesz w załączniku powyżej. Skopiuj z niego wartości inflacji dla każdego miesiąca.'''
 
+from inflation import *
+# nadanie zmiennych
 wysokosc_poczatkowa_kredytu = int(input('Podaj wartość początkową kredytu: '))
-oprocentowanie_kredytu = int(input('Podaj jakie jest oprocentowanie kredytu: '))
-kwota_raty = int(input('Podaj kwotę raty: '))
+loan_rate = int(input('Podaj oprocentowanie kredytu: '))
+kwota_raty = int(input('Podaj stałą wartość raty: '))
 
+# algorytm wyliczający kwotę pozostałą do spłaty
+#styczen
+interest = loan_rate + january
+amount_to_be_repaid = wysokosc_poczatkowa_kredytu - (kwota_raty + interest / 100)
+print('Twoja pozostała kwota kredytu to {:.2f}'.format(amount_to_be_repaid))
 
-a = oprocentowanie_kredytu - inflacja
-pozostala_kwota_kredytu = wysokosc_poczatkowa_kredytu - (kwota_raty * a)
-y =
+#luty
+interest = loan_rate + february
+amount_to_be_repaid -=  kwota_raty + interest / 100
+print('Twoja pozostała kwota kredytu to {:.2f}, to  mniej niż w poprzednim miesiącu'.format(amount_to_be_repaid,))
 
-print('Twoja pozostała kwota kredytu to {}, to y mniej niż w poprzednim miesiącu'.format(pozostala_kwota_kredytu))
+#marzec
+interest = loan_rate + march
+amount_to_be_repaid -= kwota_raty + interest / 100
+print('Twoja pozostała kwota kredytu to {:.2f}, to  mniej niż w poprzednim miesiącu'.format(amount_to_be_repaid,))
+
+TODO
+#skończyć program
+
